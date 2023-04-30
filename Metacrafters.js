727240d66b0c69608efde1e317b4ed85d82faf28
Dion Roi <ap.dionroi.flores@apecshools.edu.ph>
2023-04-30 14:25:20 +0800

    NFT

--- Metacrafters.js
+++ Metacrafters.js
@@ -1,52 +1,55 @@
-//solution for rectangle
-const resultsRectangle = (l,w)=>{
-    var perimeter = 2*(l+w);
-    var area = l*w
-    var result = {perimeter:perimeter,area:area};
-    return result
-};
+/*
+Assessment Requirements
+1. Create a variable that can hold a number of NFT's. What type of variable might this be?
+2. Create an object inside your mintNFT function that will hold the metadata for your NFTs.
+   The metadata values will be passed to the function as parameters. When the NFT is ready,
+   you will store it in the variable you created in step 1
+3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
+4. For good measure, getTotalSupply() should return the number of NFT's you have created
+*/
 
-//solution for circle
-const resultCircle = (r,pi)=>{
-    var perimeter = 2*pi*r;
-    var area = pi*(Math.pow(r,2));
-    var result = {perimeter:perimeter,area:area}
-    return result
+
+// create a variable to hold your NFT's
+const NFTs = []
+
+
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata,
+// and store it in the variable above.
+function mintNFT (_firstName, _middleName, _lastName, _bloodType) {
+    const NFT = {
+        "FirstName": _firstName,
+        "MiddleName": _middleName,
+        "LastName": _lastName,
+        "BloodType": _bloodType
+    }
+    NFTs.push(NFT);
+    console.log("Minted: " + _firstName);
 }
 
-//change values here
-var rectangle = {
-    name:"card",
-    length: 5,
-    width: 5
-};
 
-var circle = {
-    name:"plate",
-    raduis: 3.25,
-    pi: 3.14159
-};
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
+function listNFTs () {
+for(let i =0; i < NFTs.length; i++) {
+    console.log("\nPerson: \t\t" + (i + 1));
+    console.log("First Name: \t\t" + NFTs[i].FirstName);
+    console.log("Middle Name: \t\t" + NFTs[i].MiddleName);
+    console.log("Last Name: \t\t" + NFTs[i].LastName);
+    console.log("Blood Type: \t\t" + NFTs[i].BloodType);
+ }
+}
 
-//this returns true or false
-var isSquare = rectangle.length==rectangle.width
 
-//aranging values here
-const permiterRectangle = resultsRectangle(rectangle.length,rectangle.width);
-const perimeterCircle = resultCircle(circle.raduis,circle.pi);
+// print the total number of NFTs we have minted to the console
+function getTotalSupply() {
+console.log("\n"+NFTs.length);
+}
 
-const outputRectangle = {
-    "Name":rectangle.name,
-    "Area":permiterRectangle.area,
-    "Perimeter":permiterRectangle.perimeter,
-    "isSquare":isSquare
-};
 
-const outputCircle = {
-    "Name":circle.name,
-    "Area":perimeterCircle.area,
-    "Perimeter":perimeterCircle.perimeter
-};
-
-//outputs to console
-console.log ("rectangle",outputRectangle)
-console.log("circle",outputCircle)
+// call your functions below this line
+mintNFT("Kin", "Su", "Zhitsu", "A+");
+mintNFT("Tsu", "Bi", "Kimetsu", "O+");
+mintNFT("Mou", "Rou", "Kabira", "B");
+listNFTs();
+getTotalSupply();
